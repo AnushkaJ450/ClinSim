75922c024e04f77e4664acc4118733d6b31fb3de
Anushka <zcakjit@ucl.ac.uk>
2026-02-26 12:04:19 +0000

    Rd files

---
+++ R/ClinSim-package.R
@@ -0,0 +1,13 @@
+#' ClinSim:
+#'
+#' @description Describe
+#'
+#' @details The main functions are
+#'
+#'
+#' The following example datasets are provided.
+#'
+#' @docType package
+#' @aliases ClinSim-package
+#' @importFrom graphics plot
+"_PACKAGE"
